2993cb6369762276fc1a333c6969b0e690d11b58
tr3nt0n <82298389+EugenioBarbieriViale@users.noreply.github.com>
2022-07-04 21:19:35 +0200

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -36,5 +36,7 @@ mv .bashrc ~
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+source ~/.bashrc
 echo "Set up completed"
+
 exit
