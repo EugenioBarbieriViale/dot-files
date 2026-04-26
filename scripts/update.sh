f456400eda09cb3820a9441f73c6ac25f646b8cb
Eugenio <eugenio.barbieri.viale@gmail.com>
2026-04-26 14:24:02 +0200

    faulty update.sh fixed

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,16 +9,18 @@ if [[ "$DAY" == "$UPDATE_DAY" ]]; then
     touch "$LOG_FILE"
 
     if [[ "$(tail -1 "$LOG_FILE")" == "$LOG_LINE" ]]; then
-        exit 0
+        return 0
     fi
 
     echo "Starting update..."
     if sudo pacman -Syu && yay; then
         echo "$LOG_LINE" >> "$LOG_FILE"
         echo "Update complete. Logged $LOG_LINE to $LOG_FILE."
-        exit 0
+        return 0
     else
         echo "Update failed. Not logging date so it will retry next run."
-        exit 1
+        return 1
     fi
 fi
+
+return 0
